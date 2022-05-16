7791c607f00cfd6d077b3014e3f25d22df581a11
Netnarin <105627342+Netnarin@users.noreply.github.com>
2022-05-16 22:27:47 +0700

    Create actual_result_test.ts
    Create Code

---
+++ tests/actual_result_test.ts
@@ -0,0 +1,46 @@
+import "mocha";
+import "should";
+
+import { actual_result } from "../src/actual_result";
+
+describe("actual_result", () => {
+
+    let tested: actual_result;
+
+    beforeEach(() => tested = new actual_result());
+
+    describe("Original material in tonnes", () => {
+
+        it("Original material in tonnes, 2022", () => {
+            const result = tested.actual_result(1000,25,100);
+            const expected = 1000;
+            result.should.be.equal(expected);
+        });
+
+        it("Original material in tonnes, 2023", () => {
+            const result = tested.actual_result(1500,25,100);
+            const expected = 1500;
+            result.should.be.equal(expected);
+        });
+        
+        it("Original material in tonnes, 2024", () => {
+            const result = tested.actual_result(450,25,100);
+            const expected = 900;
+            result.should.be.equal(expected);
+        });
+
+        it("Original material in tonnes, 2025", () => {
+            const result = tested.actual_result(500,25,100);
+            const expected = 1000;
+            result.should.be.equal(expected);
+        });
+
+
+        it("Original material in tonnes, 2026", () => {
+            const result = tested.actual_result(600,25,100);
+            const expected = 1200;
+            result.should.be.equal(expected);
+        });
+    });
+
+});

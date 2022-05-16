e4b527a4fe0d2f184adbac2a36973e483b6d414b
Netnarin <105627342+Netnarin@users.noreply.github.com>
2022-05-16 23:02:13 +0700

    Update actual_result_test.ts
    add for * comment

--- tests/actual_result_test.ts
+++ tests/actual_result_test.ts
@@ -9,56 +9,56 @@ describe("actual_result", () => {
 
     beforeEach(() => tested = new actual_result());
 
-    describe("Test Case #1", () => {
-        it("Original material in tonnes, 2022", () => {
+    describe("Test Case #1 ( 50% )", () => {
+        it("Original material in tonnes, 2022 : 1000 *( False )", () => {
             const result = tested.actual_result(1000,50,100);
             const expected = 1000;
             result.should.be.equal(expected);
         });
-        it("Original material in tonnes, 2023", () => {
+        it("Original material in tonnes, 2023 : 1500 *( False )", () => {
             const result = tested.actual_result(1500,50,100);
             const expected = 1500;
             result.should.be.equal(expected);
         });        
-        it("Original material in tonnes, 2024", () => {
+        it("Original material in tonnes, 2024 : 450", () => {
             const result = tested.actual_result(450,50,100);
             const expected = 900;
             result.should.be.equal(expected);
         });
-        it("Original material in tonnes, 2025", () => {
+        it("Original material in tonnes, 2025 : 500", () => {
             const result = tested.actual_result(500,50,100);
             const expected = 1000;
             result.should.be.equal(expected);
         });
-        it("Original material in tonnes, 2026", () => {
+        it("Original material in tonnes, 2026 : 600", () => {
             const result = tested.actual_result(600,50,100);
             const expected = 1200;
             result.should.be.equal(expected);
         });
     });
     
-    describe("Test Case #2", () => {
-        it("Original material in tonnes, 2022", () => {
+    describe("Test Case #2 ( 25% )", () => {
+        it("Original material in tonnes, 2022 : 700 *( False )", () => {
             const result = tested.actual_result(700,25,100);
             const expected = 1000;
             result.should.be.equal(expected);
         });
-        it("Original material in tonnes, 2023", () => {
+        it("Original material in tonnes, 2023 : 375", () => {
             const result = tested.actual_result(375,25,100);
             const expected = 1500;
             result.should.be.equal(expected);
         });        
-        it("Original material in tonnes, 2024", () => {
+        it("Original material in tonnes, 2024 : 225", () => {
             const result = tested.actual_result(225,25,100);
             const expected = 900;
             result.should.be.equal(expected);
         });
-        it("Original material in tonnes, 2025", () => {
+        it("Original material in tonnes, 2025 : 250", () => {
             const result = tested.actual_result(250,25,100);
             const expected = 1000;
             result.should.be.equal(expected);
         });
-        it("Original material in tonnes, 2026", () => {
+        it("Original material in tonnes, 2026 : 300", () => {
             const result = tested.actual_result(300,25,100);
             const expected = 1200;
             result.should.be.equal(expected);
